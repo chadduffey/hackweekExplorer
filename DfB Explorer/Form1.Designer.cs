a4704448ed6813d169c2b3264d4c1bf91b4b09c4
Chad Duffey <chadduffey@hotmail.com>
2015-08-18 13:54:20 +1000

    user tree populated

---
+++ DfB Explorer/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace DfB_Explorer
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.treeViewUsers = new System.Windows.Forms.TreeView();
+            this.treeViewFiles = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.pictureBoxConnected = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.labelTeamName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxConnected)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeViewUsers
+            // 
+            this.treeViewUsers.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.treeViewUsers.ImageIndex = 3;
+            this.treeViewUsers.ImageList = this.imageList1;
+            this.treeViewUsers.Location = new System.Drawing.Point(5, 43);
+            this.treeViewUsers.Name = "treeViewUsers";
+            this.treeViewUsers.SelectedImageIndex = 3;
+            this.treeViewUsers.Size = new System.Drawing.Size(333, 569);
+            this.treeViewUsers.TabIndex = 0;
+            // 
+            // treeViewFiles
+            // 
+            this.treeViewFiles.Location = new System.Drawing.Point(345, 43);
+            this.treeViewFiles.Name = "treeViewFiles";
+            this.treeViewFiles.Size = new System.Drawing.Size(394, 569);
+            this.treeViewFiles.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Token";
+            // 
+            // txtToken
+            // 
+            this.txtToken.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtToken.Location = new System.Drawing.Point(50, 11);
+            this.txtToken.Name = "txtToken";
+            this.txtToken.PasswordChar = '*';
+            this.txtToken.Size = new System.Drawing.Size(217, 25);
+            this.txtToken.TabIndex = 3;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonConnect.Location = new System.Drawing.Point(273, 8);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(65, 29);
+            this.buttonConnect.TabIndex = 4;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // pictureBoxConnected
+            // 
+            this.pictureBoxConnected.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxConnected.Image")));
+            this.pictureBoxConnected.Location = new System.Drawing.Point(345, 8);
+            this.pictureBoxConnected.Name = "pictureBoxConnected";
+            this.pictureBoxConnected.Size = new System.Drawing.Size(52, 28);
+            this.pictureBoxConnected.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxConnected.TabIndex = 5;
+            this.pictureBoxConnected.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "tick");
+            this.imageList1.Images.SetKeyName(1, "unplugged");
+            this.imageList1.Images.SetKeyName(2, "user.png");
+            this.imageList1.Images.SetKeyName(3, "cloudusericon.png");
+            // 
+            // labelTeamName
+            // 
+            this.labelTeamName.AutoSize = true;
+            this.labelTeamName.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTeamName.Location = new System.Drawing.Point(403, 14);
+            this.labelTeamName.Name = "labelTeamName";
+            this.labelTeamName.Size = new System.Drawing.Size(86, 17);
+            this.labelTeamName.TabIndex = 6;
+            this.labelTeamName.Text = "Disconnected";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 618);
+            this.Controls.Add(this.labelTeamName);
+            this.Controls.Add(this.pictureBoxConnected);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.txtToken);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeViewFiles);
+            this.Controls.Add(this.treeViewUsers);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.Text = "Dropbox for Business File Explorer";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxConnected)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeViewUsers;
+        private System.Windows.Forms.TreeView treeViewFiles;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtToken;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.PictureBox pictureBoxConnected;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Label labelTeamName;
+    }
+}
+
---
+++ DfB Explorer/Form1.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace DfB_Explorer
+{
+    public partial class FormMain : Form
+    {
+        
+        [DataContract]
+        class BasicTeamInformation
+        {
+            //folder listing:
+            [DataMember]
+            public string name { get; set; }
+            [DataMember]
+            public string team_id { get; set; }
+            [DataMember]
+            public int num_licenced_users { get; set; }
+            [DataMember]
+            public int num_provisioned_users { get; set; }
+        }
+
+        [DataContract]
+        class Profile
+        {
+            //profile:
+            [DataMember]
+            public string given_name { get; set; }
+            [DataMember]
+            public string surname { get; set; }
+            [DataMember]
+            public string status { get; set; }
+            [DataMember]
+            public string member_id { get; set; }
+            [DataMember]
+            public string email { get; set; }
+            [DataMember]
+            public string external_id { get; set; }
+            [DataMember]
+            public List<string> groups { get; set; }
+        }
+
+        [DataContract]
+        class Permissions
+        {
+            //permissions:
+            [DataMember]
+            public bool is_admin { get; set; }
+        }
+
+        [DataContract]
+        class Member
+        //member is made up of profile and permissions components
+        {
+            [DataMember]
+            public Profile profile { get; set; }
+            [DataMember]
+            public Permissions permissions { get; set; }
+        }
+
+        [DataContract]
+        class DfBTeam
+        {
+            //team is made up of members and has a cursor and has_more attribute
+            [DataMember]
+            public List<Member> members { get; set; }
+            [DataMember]
+            public string cursor { get; set; }
+            [DataMember]
+            public bool has_more { get; set; }
+        }
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            //temporary for testing:
+            //txtToken.Text = "5POVRTzm3ZAAAAAAAAABwcZwXMCSEGQepWk7GDKjb_1yr_C7xMifXgE7QP7kv7Pi"; //dbtests.info
+            txtToken.Text = "32Wp8V0dZ28AAAAAAADJlETVJXSVl4MdK_vjaDRxqFZ8_Id_qdbZPt0JoTOmfglU"; //hanfordinc.com
+
+            pictureBoxConnected.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBoxConnected.Image = imageList1.Images[1];
+
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            // Set cursor as hourglass
+            Cursor.Current = Cursors.WaitCursor;
+            
+            //obtain basic api information
+            BasicTeamInformation teamInformation = dfbBasicTeamInfo();
+            //obtain team information
+            DfBTeam dfbTeam = dfb_members_list();
+
+            //if we connect to the api successfully:
+            if (teamInformation.num_licenced_users >= 0)
+            {
+                labelTeamName.Text = "Connected to: " + teamInformation.name;
+                pictureBoxConnected.SizeMode = PictureBoxSizeMode.Zoom;
+                pictureBoxConnected.Image = imageList1.Images[0];
+                populateDfBMembers(dfbTeam);
+            }
+
+
+            // Set cursor as default arrow
+            Cursor.Current = Cursors.Default;    
+        }
+
+        private void populateDfBMembers(DfBTeam dfbTeam)
+        {
+            int totalMembers = dfbTeam.members.Count;
+            for (int i = 0; i < totalMembers; i++)
+            {
+                string fullname = dfbTeam.members[i].profile.given_name + " " + dfbTeam.members[i].profile.surname;
+                if (fullname != " ")
+                {
+                    if (dfbTeam.members[i].profile.status == "active")
+                    {
+                        TreeNode treeNode = new TreeNode(fullname);
+                        treeViewUsers.Nodes.Add(treeNode);
+                    }
+                }
+            }
+        }
+
+        private BasicTeamInformation dfbBasicTeamInfo()
+        {
+            String page = "https://api.dropbox.com/1/team/get_info";
+            WebRequest request = WebRequest.Create(page);
+
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            String authheader = "Authorization:Bearer " + txtToken.Text;
+            request.Headers.Add(authheader);
+
+            // Create POST data and convert it to a byte array.
+            string postData = "{}";
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+            // Set the ContentLength property of the WebRequest.
+            request.ContentLength = byteArray.Length;
+
+            // Get the request stream.
+            Stream ds = request.GetRequestStream();
+
+            // Write the data to the request stream.
+            ds.Write(byteArray, 0, byteArray.Length);
+
+            // Close the Stream object.
+            ds.Close();
+
+            // Get the response.
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                // Get the stream containing content returned by the server.
+                Stream dataStream = response.GetResponseStream();
+
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+
+                // Read the content. 
+                string responseFromServer = reader.ReadToEnd();
+
+                // Cleanup the streams and the response.
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+
+                //serialise the json so we can send back a BasicTeamInformation object
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(BasicTeamInformation));
+                MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(responseFromServer));
+                BasicTeamInformation teamObj = (BasicTeamInformation)ser.ReadObject(stream);
+
+                return teamObj;
+            }
+
+            catch (WebException error)
+            {
+                BasicTeamInformation failedObj = new BasicTeamInformation();
+                failedObj.name = error.Message;
+                failedObj.num_licenced_users = -1;
+                failedObj.num_provisioned_users = -1;
+                return failedObj;
+            }
+        }
+
+        private DfBTeam dfb_members_list()
+        {
+            String page = "https://api.dropbox.com/1/team/members/list";
+            WebRequest request = WebRequest.Create(page);
+
+            request.Method = "POST";
+            request.ContentType = "application/json";
+            String authheader = "Authorization:Bearer " + txtToken.Text;
+            request.Headers.Add(authheader);
+
+            // Create POST data and convert it to a byte array.
+            string postData = "{}";
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+            // Set the ContentLength property of the WebRequest.
+            request.ContentLength = byteArray.Length;
+
+            // Get the request stream.
+            Stream ds = request.GetRequestStream();
+
+            // Write the data to the request stream.
+            ds.Write(byteArray, 0, byteArray.Length);
+
+            // Close the Stream object.
+            ds.Close();
+
+            // Get the response.
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                // Get the stream containing content returned by the server.
+                Stream dataStream = response.GetResponseStream();
+
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+
+                // Read the content. 
+                string responseFromServer = reader.ReadToEnd();
+
+                // Cleanup the streams and the response.
+                reader.Close();
+                dataStream.Close();
+                response.Close();
+
+                //serialise the json so we can send back a BasicTeamInformation object
+                DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(DfBTeam));
+                MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(responseFromServer));
+                DfBTeam teamObj = (DfBTeam)ser.ReadObject(stream);
+
+                return teamObj;
+            }
+
+            catch (WebException error)
+            {
+                DfBTeam failedObj = new DfBTeam();
+                failedObj.cursor = error.Message;
+                return failedObj;
+            }
+
+        }
+
+
+    }
+}
